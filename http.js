aeb80b85e11d7d730027c0f1cacbade3713991ce
Seymour Poler <seymourpoler@gmail.com>
2018-09-10 21:44:34 +0100

    adding refactored library

--- http.js
+++ http.js
@@ -1,70 +1,76 @@
-function Http(){
-    const contentTypeHeader  = 'Content-type';
-    const contentTypeJson = 'application/json; charset=utf-8';
+(function (WiMi) {
+    function Http() {
+        const contentTypeHeader = 'Content-Type';
+        const contentTypeJson = 'application/json;charset=UTF-8';
 
-    var self = this;
-    var xmlHttpRequest = new XMLHttpRequest();
+        var self = this;
 
-    self.get = function(url, successHandler, errorHandler){
-        xmlHttpRequest.onreadystatechange = function () {
+        self.get = function (url, successHandler, errorHandler) {
+            var xmlHttpRequest = buildXmlHttpRequest('GET', url, successHandler, errorHandler);
+            xmlHttpRequest.send();
+        };
+
+        self.post = function (url, request, successHandler, errorHandler) {
+            var xmlHttpRequest = buildXmlHttpRequest('POST', url, successHandler, errorHandler);
+            xmlHttpRequest.send(JSON.stringify(request));
+        };
+
+        self.put = function (url, request, successHandler, errorHandler) {
+            var xmlHttpRequest = buildXmlHttpRequest('PUT', url, successHandler, errorHandler);
+            xmlHttpRequest.send(JSON.stringify(request));
+        };
+
+        self.delete = function (url, successHandler, errorHandler) {
+            var xmlHttpRequest = buildXmlHttpRequest('DELETE', url, successHandler, errorHandler);
+            xmlHttpRequest.send();
+        };
+
+        function buildXmlHttpRequest(httpVerb, url, successHandler, errorHandler) {
+            var xmlHttpRequest = new XMLHttpRequest();
+            xmlHttpRequest.onreadystatechange = function () {
+                xmlHttpRequestHandler(httpVerb, url, xmlHttpRequest, successHandler, errorHandler);
+            }
+            xmlHttpRequest.open(httpVerb, url, true);
+            xmlHttpRequest.setRequestHeader(contentTypeHeader, contentTypeJson);
+            return xmlHttpRequest;
+        }
+
+        function xmlHttpRequestHandler(httpVerb, url, xmlHttpRequest, successHandler, errorHandler) {
             if (isOk(xmlHttpRequest)) {
-                successHandler(xmlHttpRequest);
+                handleResponse(xmlHttpRequest.response, successHandler);
                 return;
             }
-            errorHandler(xmlHttpRequest);
-            return;
-        }
-        xmlHttpRequest.open('GET', url, true);
-        xmlHttpRequest.setRequestHeader(contentTypeHeader, contentTypeJson);
-        xmlHttpRequest.send();
-    };
-
-    self.post = function(url, request, successHandler, errorHandler){
-        xmlHttpRequest.onreadystatechange = function () {
-            if (isOk(xmlHttpRequest)) {
-                successHandler(xmlHttpRequest);
+            if (isAnError(xmlHttpRequest)) {
+                handleResponse(xmlHttpRequest.response, errorHandler);
                 return;
             }
-            errorHandler(xmlHttpRequest);
-            return;
-        }
-        xmlHttpRequest.open('POST', url, true);
-        xmlHttpRequest.setRequestHeader(contentTypeHeader, contentTypeJson);
-        xmlHttpRequest.send(JSON.stringify(request));
-    };
+            console.log('http ' + httpVerb + ': ' + url + ' with unkown response: ', xmlHttpRequest);
 
-    self.put = function(url, request, successHandler, errorHandler){
-        xmlHttpRequest.onreadystatechange = function () {
-            if (isOk(xmlHttpRequest)) {
-                successHandler(xmlHttpRequest);
-                return;
+            function handleResponse(response, handler) {
+                if (response === '') {
+                    handler(response);
+                    return;
+                }
+                const responseParsed = JSON.parse(response);
+                handler(responseParsed);
             }
-            errorHandler(xmlHttpRequest);
-            return;
         }
-        xmlHttpRequest.open('PUT', url, true);
-        xmlHttpRequest.setRequestHeader(contentTypeHeader, contentTypeJson);
-        xmlHttpRequest.send(JSON.stringify(request));
-    };
 
-    self.delete = function(url, successHandler, errorHandler){
-        xmlHttpRequest.onreadystatechange = function () {
-            if (isOk(xmlHttpRequest)) {
-                successHandler(xmlHttpRequest);
-                return;
-            }
-            errorHandler(xmlHttpRequest);
-            return;
+        function isOk(response) {
+            const ready = 4;
+            const ok = 200;
+            return response.readyState == ready &&
+                response.status == ok;
         }
-        xmlHttpRequest.open('DELETE', url, true);
-        xmlHttpRequest.setRequestHeader(contentTypeHeader, contentTypeJson);
-        xmlHttpRequest.send();
-    };
 
-    function isOk(response){
-        const ready = 4;
-        const ok = 200;
-        return response.readyState == ready && 
-            response.status == ok;
+        function isAnError(response) {
+            const internalServerErrorTypeNumber = 5;
+            const requestErrorTypeNumber = 4;
+            var divisionResult = response.status / 100;
+            return divisionResult == internalServerErrorTypeNumber ||
+                divisionResult == requestErrorTypeNumber;
+        }
     }
-}
+    WiMi.Http = Http;
+
+})(WiMi || {})
